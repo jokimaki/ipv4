4b022ba75de340f14dde13c698d263b23a676abe
Juha Jokimäki <juha.jokimaki@iki.fi>
2015-08-29 18:41:11 +0300

    Added format-address

--- src/jokimaki/ipv4.clj
+++ src/jokimaki/ipv4.clj
@@ -13,3 +13,13 @@
       (if (seq digits)
         (recur (bit-or (bit-shift-left ip 8) (first digits)), (rest digits))
         ip))))
+
+(defn ^String format-address [^Long ip]
+  {:pre [(number? ip)]}
+  (when-not (<= 0 ip 0xffffffff) (throw (IllegalArgumentException. (str "Out of IPv4 range"))))
+
+  (apply format "%d.%d.%d.%d"
+    (loop [result '(), ip-bits ip]
+      (if (>= (count result) 4)
+        result
+        (recur (conj result (bit-and 0xff ip-bits)) (bit-shift-right ip-bits 8))))))
--- test/jokimaki/ipv4_test.clj
+++ test/jokimaki/ipv4_test.clj
@@ -23,3 +23,39 @@
     (is (thrown? IllegalArgumentException (parse-address "0.0.0.+1")))
     (is (thrown? IllegalArgumentException (parse-address "0.0.0.a")))
     (is (thrown? IllegalArgumentException (parse-address "0.0.0.256")))))
+
+(deftest test-format-address
+  (testing "simple valid addresses"
+    (is (= "0.0.0.0") (format-address 0))
+    (is (= "0.0.0.1") (format-address 1))
+    (is (= "0.0.1.0") (format-address 0x100))
+    (is (= "0.1.0.0") (format-address 0x10000))
+    (is (= "1.0.0.0") (format-address 0x1000000))
+    (is (= "0.0.0.255") (format-address 0xff))
+    (is (= "255.255.255.255") (format-address 0xffffffff))
+    (is (= "0.255.1.255" (format-address 0xff01ff))))
+
+  (testing "invalid addresses"
+    (is (thrown? AssertionError (format-address nil)))
+    (is (thrown? IllegalArgumentException (format-address -1)))
+    (is (thrown? IllegalArgumentException (format-address Long/MAX_VALUE)))
+    (is (thrown? IllegalArgumentException (format-address Long/MIN_VALUE)))
+    (is (thrown? IllegalArgumentException (format-address Integer/MIN_VALUE)))
+    (is (thrown? IllegalArgumentException (format-address (+ 2 (* 2 Integer/MAX_VALUE)))))
+    (is (thrown? IllegalArgumentException (format-address 0x100000000)))))
+
+(deftest test-parse-format-combination
+  (let [pf (comp format-address parse-address)
+        fp (comp parse-address format-address)]
+
+    (testing "parse->format"
+      (is (= "0.0.0.0" (pf "0.0.0.0")))
+      (is (= "172.16.0.1" (pf "172.16.0.1")))
+      (is (= "192.168.100.8" (pf "192.168.100.8")))
+      (is (= "255.255.255.255" (pf "255.255.255.255"))))
+
+    (testing "format->parse"
+      (is (= 0 (fp 0)))
+      (is (= 123456 (fp 123456)))
+      (is (= 12345678 (fp 12345678)))
+      (is (= 0xffffffff (fp 0xffffffff))))))
